90d564a23bcb19498482f2cbf7f0c06c6dca06f5
SamuilG <samuilgeorgiev@abv.bg>
2020-10-11 13:49:21 +0300

    gps better?

--- js/index.js
+++ js/index.js
@@ -16,8 +16,8 @@ function onError(error) {
 }
 
 function getPosition(position) {
-    let x = position.coords.latitude;
-    let y = position.coords.longitude;
+    let x = position.coords.latitude.toFixed(6);
+    let y = position.coords.longitude.toFixed(6);
     let z = 13;
     runPosition(x, y, z);
 }
@@ -26,7 +26,7 @@ function defaltPosition(){
     let x = 42.703;
     let y = 23.30;
     let z = 6;
-    runPosition(x, y, z);
+    runDefaltPosition(x, y, z);
 }
 
 function runPosition(x, y, z) {
@@ -42,11 +42,28 @@ function runPosition(x, y, z) {
     let  map  =  L.map('map').setView([x,  y],  z);  // z=zoom
     map.addLayer(osmLayer).addLayer(waqiLayer);
 
-    var circle = L.circle([x, y], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
+    let marker = L.marker([x, y]).addTo(map);
+
+    let circle = L.circle([x, y], {
+        // color: 'blue',
+        fillColor: 'blue',
+        fillOpacity: 0.25,
         radius: 500
     }).addTo(map);
+
+}
+
+function runDefaltPosition(x, y, z) {
+
+    let  OSM_URL  =  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';  
+    let  OSM_ATTRIB  =  '&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';  
+    let  osmLayer  =  L.tileLayer(OSM_URL,  {attribution:  OSM_ATTRIB});  
+
+    let  WAQI_URL    =  "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";  
+    let  WAQI_ATTR  =  'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';  
+    let  waqiLayer  =  L.tileLayer(WAQI_URL,  {attribution:  WAQI_ATTR});  
+
+    let  map  =  L.map('map').setView([x,  y],  z);  // z=zoom
+    map.addLayer(osmLayer).addLayer(waqiLayer);
 
 }
