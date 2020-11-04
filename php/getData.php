1acab2ae4d4ddf7061d2fa0583395b57775894b2
ilko27 <ilko.petrov27@gmail.com>
2020-11-04 19:26:20 +0200

    Java Script is a very stupid language. I rage quit for today

---
+++ php/getData.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "ilko";
+$password = "ilko";
+$db = "healther";
+
+$tempArray = [];
+
+$conn = new mysqli($servername, $username, $password, $db);
+$stmt = $conn->prepare("SELECT * FROM sensordata");
+$stmt->execute();
+$result = $stmt->get_result();
+while($row = mysqli_fetch_array($result)){
+    array_push($tempArray, $row['temperatureC']);
+}
+
+echo json_encode($tempArray);
+
+?>
