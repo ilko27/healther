3b98cc0a3be9a647f05a6a3fd9a9d10612abe85f
SamuilG <samuilgeorgiev@abv.bg>
2020-11-06 11:11:27 +0200

    fuck work

--- index.php
+++ index.php
@@ -11,24 +11,6 @@
 </head>
 <body>
 
-    <!-- <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div class="container">
-          <a class="navbar-brand" href="#">Healther</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="html/signup.html">Sign Up</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="html/signin.html">Log In</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> -->
 
     <header>
         <!-- <div class="header-right">
