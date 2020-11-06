0c38a1ee0e73f9ef3de31b2504985e43beb47bd2
SamuilG <samuilgeorgiev@abv.bg>
2020-11-06 11:20:39 +0200

    healther

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/cut_clouds.png" type="image/gif" sizes="16x16">
+    <link rel="icon" href="images/healther.png" type="image/gif" sizes="16x16">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.css">
     <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
