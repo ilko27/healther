1881053a19657e8b23fe928818f9b303b5055196
ilko27 <ilko.petrov27@gmail.com>
2020-11-06 10:31:50 +0200

    Stupid JSON

--- php/getData.php
+++ php/getData.php
@@ -7,7 +7,7 @@ $db = "healther";
 $tempArray = [];
 
 $conn = new mysqli($servername, $username, $password, $db);
-$stmt = $conn->prepare("SELECT * FROM sensordata");
+$stmt = $conn->prepare("SELECT * FROM sensordata WHERE sensor = 'IlkoSensor'");
 $stmt->execute();
 $result = $stmt->get_result();
 while($row = mysqli_fetch_array($result)){
