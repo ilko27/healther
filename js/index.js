49a65336c8e6b24964bfbf980060bc33a7b579f6
SamuilG <samuilgeorgiev@abv.bg>
2020-11-07 17:36:57 +0200

    auto map accuracy and error messages

--- js/index.js
+++ js/index.js
@@ -1,25 +1,49 @@
+let error_map;
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getPosition, onError);
     } else { 
         defaltPosition();
+        error_map = "Браузърът не поддържа местополижениен."
+        alert(error_map);
     }
 }
 getLocation();
 
 function onError(error) {
+    // switch(error.code) {
+    //     case error.PERMISSION_DENIED || error.POSITION_UNAVAILABLE || error.TIMEOUT || error.UNKNOWN_ERROR:
+    //     defaltPosition();
+    //     break;
+    // }
     switch(error.code) {
-        case error.PERMISSION_DENIED || error.POSITION_UNAVAILABLE || error.TIMEOUT || error.UNKNOWN_ERROR:
-        defaltPosition();
-        break;
+        case error.PERMISSION_DENIED:
+            defaltPosition();
+            error_map = "Достъпът го местополижение е отказан."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            defaltPosition();
+            error_map = "Няма налично местоположение."
+            break;
+        case error.TIMEOUT:
+            defaltPosition();
+            error_map = "Времето за предоставяне на достъп до местоположението изтече."
+            break;
+        case error.UNKNOWN_ERROR:
+            defaltPosition();
+            error_map = "Неизвестна грешка. Стискам ти палци."
+            break;
     }
+    alert(error_map);
 }
 
 function getPosition(position) {
     let x = position.coords.latitude.toFixed(6);
     let y = position.coords.longitude.toFixed(6);
+    let accuracy = position.coords.accuracy;
     let z = 13;
-    runPosition(x, y, z);
+    runPosition(x, y, z, accuracy);
 }
 
 function defaltPosition(){
@@ -29,17 +53,17 @@ function defaltPosition(){
     runDefaltPosition(x, y, z);
 }
 
-function runPosition(x, y, z) {
+function runPosition(x, y, z, accuracy) {
 
-    let  OSM_URL  =  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';  
-    let  OSM_ATTRIB  =  '&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';  
-    let  osmLayer  =  L.tileLayer(OSM_URL,  {attribution:  OSM_ATTRIB});  
+    let OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';  
+    let OSM_ATTRIB = '&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';  
+    let osmLayer = L.tileLayer(OSM_URL,  {attribution:  OSM_ATTRIB});  
 
-    let  WAQI_URL    =  "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";  
-    let  WAQI_ATTR  =  'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';  
-    let  waqiLayer  =  L.tileLayer(WAQI_URL,  {attribution:  WAQI_ATTR});  
+    let WAQI_URL = "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";  
+    let WAQI_ATTR = 'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';  
+    let waqiLayer = L.tileLayer(WAQI_URL,  {attribution:  WAQI_ATTR});  
 
-    let  map  =  L.map('map').setView([x,  y],  z);  // z=zoom
+    let map = L.map('map').setView([x,  y],  z);  // z=zoom
     map.addLayer(osmLayer).addLayer(waqiLayer);
 
     let marker = L.marker([x, y]).addTo(map);
@@ -48,22 +72,22 @@ function runPosition(x, y, z) {
         // color: 'blue',
         fillColor: 'blue',
         fillOpacity: 0.25,
-        radius: 500
+        radius: accuracy
     }).addTo(map);
 
 }
 
 function runDefaltPosition(x, y, z) {
 
-    let  OSM_URL  =  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';  
-    let  OSM_ATTRIB  =  '&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';  
-    let  osmLayer  =  L.tileLayer(OSM_URL,  {attribution:  OSM_ATTRIB});  
+    let  OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';  
+    let  OSM_ATTRIB = '&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';  
+    let  osmLayer = L.tileLayer(OSM_URL,  {attribution:  OSM_ATTRIB});  
 
-    let  WAQI_URL    =  "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";  
-    let  WAQI_ATTR  =  'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';  
-    let  waqiLayer  =  L.tileLayer(WAQI_URL,  {attribution:  WAQI_ATTR});  
+    let  WAQI_URL = "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";  
+    let  WAQI_ATTR = 'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';  
+    let  waqiLayer = L.tileLayer(WAQI_URL,  {attribution:  WAQI_ATTR});  
 
-    let  map  =  L.map('map').setView([x,  y],  z);  // z=zoom
+    let  map = L.map('map').setView([x,  y],  z);  // z=zoom
     map.addLayer(osmLayer).addLayer(waqiLayer);
 
 }
