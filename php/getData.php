a288f9b7301a1d3771685b823a287fffe4a9b9f9
SamuilG <samuilgeorgiev@abv.bg>
2020-11-07 10:12:45 +0200

    php who?

--- php/getData.php
+++ php/getData.php
@@ -1,18 +1,38 @@
 <?php
 $servername = "localhost";
-$username = "ilko";
-$password = "ilko";
-$db = "healther";
+$username = "health645_samuil";
+$password = "Samuil_2003";
+$db = "health645_healther";
 
-$inInfo = json_decode(file_get_contents("php://input"));
-$sensorName = $inInfo->sensorName;
+// $inInfo = json_decode(file_get_contents("php://input"));
+// $sensorName = $inInfo->sensorName;
+$sensorName = 'IlkoSensor';
 
-$conn = new mysqli($servername, $username, $password, $db);
-$stmt = $conn->prepare("SELECT * FROM sensordata WHERE sensor = ?");
-$stmt->bind_param("s", $sensorName);
-$stmt->execute();
-$result = $stmt->get_result();
-$outp = $result->fetch_all(MYSQLI_ASSOC);
+// $conn = new mysqli($servername, $username, $password, $db);
+// $stmt = $conn->prepare("SELECT * FROM sensordata WHERE sensor = ?");
+// $stmt->bind_param("s", $sensorName);
+// $stmt->execute();
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+$sql = "SELECT * FROM sensorData WHERE sensor = ?";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, 's', $sensorName);
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+$outp = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// mysqli_stmt_bind_result($stmt, $datId, $sensor, $temperatureC, $pressure, $humidity, $readingTime);
+// while (mysqli_stmt_fetch($stmt)) {
+//     echo ('<br> д');
+// }
+
+// $result = $stmt->get_result();
+// $outp = $result->fetch_all(MYSQLI_ASSOC);
 
 
 echo json_encode($outp);
