8e2b85b01b997d416caf0ff5ea090325cef18592
SamuilG <samuilgeorgiev@abv.bg>
2020-11-07 10:15:24 +0200

    php no testing area

--- php/getData.php
+++ php/getData.php
@@ -4,14 +4,8 @@ $username = "health645_samuil";
 $password = "Samuil_2003";
 $db = "health645_healther";
 
-// $inInfo = json_decode(file_get_contents("php://input"));
-// $sensorName = $inInfo->sensorName;
-$sensorName = 'IlkoSensor';
-
-// $conn = new mysqli($servername, $username, $password, $db);
-// $stmt = $conn->prepare("SELECT * FROM sensordata WHERE sensor = ?");
-// $stmt->bind_param("s", $sensorName);
-// $stmt->execute();
+$inInfo = json_decode(file_get_contents("php://input"));
+$sensorName = $inInfo->sensorName;
 
 $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
@@ -25,14 +19,6 @@ mysqli_stmt_execute($stmt);
 
 $result = mysqli_stmt_get_result($stmt);
 $outp = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-// mysqli_stmt_bind_result($stmt, $datId, $sensor, $temperatureC, $pressure, $humidity, $readingTime);
-// while (mysqli_stmt_fetch($stmt)) {
-//     echo ('<br> д');
-// }
-
-// $result = $stmt->get_result();
-// $outp = $result->fetch_all(MYSQLI_ASSOC);
 
 
 echo json_encode($outp);
