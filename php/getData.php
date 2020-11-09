b3b856c5ebe2a9e41d1f544c7637f9f3ca084558
SamuilG <samuilgeorgiev@abv.bg>
2020-11-09 11:59:33 +0200

    lets call it a day

--- php/getData.php
+++ php/getData.php
@@ -1,20 +1,24 @@
 <?php
 require 'dbconn.php';
 
-// if (isset($inInfo)) {
+// if (window.XMLHttpRequest) {
 
-$inInfo = json_decode(file_get_contents("php://input"));
-$sensorName = $inInfo->sensorName;
+    $inInfo = json_decode(file_get_contents("php://input"));
+    $sensorName = $inInfo->sensorName;
 
-$sql = "SELECT * FROM sensorData WHERE sensor = ?";
-$stmt = mysqli_prepare($conn, $sql);
-mysqli_stmt_bind_param($stmt, 's', $sensorName);
-mysqli_stmt_execute($stmt);
+    $sql = "SELECT * FROM sensorData WHERE sensor = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 's', $sensorName);
+    mysqli_stmt_execute($stmt);
 
-$result = mysqli_stmt_get_result($stmt);
-$outp = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $result = mysqli_stmt_get_result($stmt);
+    $outp = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
-echo json_encode($outp);
+    echo json_encode($outp);
 
+// } else {
+//     header("Location: ../index.php");
+//     exit();
+// }
 ?>
