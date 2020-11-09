33192e76c84a851f31db99d545aed36f1c7b3809
SamuilG <samuilgeorgiev@abv.bg>
2020-11-09 11:40:38 +0200

    in/out

---
+++ php/signup.php
@@ -0,0 +1,23 @@
+<?php
+require 'dbconn.php';
+
+
+
+    $inInfo = json_decode(file_get_contents("php://input"));
+    $sensorName = $inInfo->sensorName;
+
+    $sql = "SELECT * FROM sensorData WHERE sensor = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, 's', $sensorName);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+    $outp = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    echo json_encode($outp);
+
+} else {
+    header("Location: ../index.php");
+    exit();
+}
+?>
