3a292298941fb75c8e1da54e21950046df11d4b6
SamuilG <samuil.georgiev@outlook.com>
2020-12-29 16:01:59 +0000

    false email report

--- pages/false_email.php
+++ pages/false_email.php
@@ -1,18 +1,17 @@
 <?php
-if (isset($_GET["email"]) && isset($_GET["code"])) {    //check url for relevant data
+if (isset($_GET["email"])) {    //check url for relevant data
     $email = $_GET["email"];
-    $code =  $_GET["code"];
 
     require '../php/dbconn.php';
 
     
-    $sql = "UPDATE users SET status = 1 WHERE email = ? AND status = ?;";  // activate user if email and code concur
+    $sql = "DELETE FROM users WHERE email = ? AND status != 1;";  // delete user if email and code concur
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header( "Location: ../index.php?error=sqlerror");
         exit();
     } else {
-        mysqli_stmt_bind_param($stmt, "ss", $email, $code);
+        mysqli_stmt_bind_param($stmt, "s", $email);
         mysqli_stmt_execute($stmt);
         if (mysqli_affected_rows($conn)!=0) {   //check if changes have been made
             $out = true;
@@ -25,6 +24,8 @@ if (isset($_GET["email"]) && isset($_GET["code"])) {    //check url for relevant
     header("Location: ../index.php");
     exit();
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -38,13 +39,12 @@ if (isset($_GET["email"]) && isset($_GET["code"])) {    //check url for relevant
         <p>
             <?php
                 if ($out == true) {
-                    echo "Your account is now active.";
+                    echo "Your email has been removed from our database.";
                 } else {
-                    echo "Unable to verify email.";
+                    echo "Unable to comply.";
                 }
             ?>
         </p>
-        <p>We will automatically redirected you in a moment.</p>
     </div>
 
 
