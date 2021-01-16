23bf20522025664697bf18f9d81f22002dc55faa
ilko27 <ilko.petrov27@gmail.com>
2021-01-16 16:39:23 +0200

    a

--- php/login.php
+++ php/login.php
@@ -9,14 +9,12 @@ $hash_pass = md5($password);
 // $hash_pass = password_hash($password, PASSWORD_DEFAULT);
 // $hash_pass = '$2y$10$XQH.viN04JTcVf2aVffgPOR3GRVav/rxh4bb/vSPh4Jx2F97Ot25K';
 
-$sql = "SELECT user_id FROM users WHERE email = ? AND pass = ? AND status = 1";
-$stmt = mysqli_stmt_init($conn); 
-mysqli_stmt_prepare($stmt, $sql);
-mysqli_stmt_bind_param($stmt, "ss", $email, $hash_pass);
-mysqli_stmt_execute($stmt);
-mysqli_stmt_store_result($stmt);
-$resultCheck = mysqli_stmt_num_rows($stmt);
-if($resultCheck != 1){
+$sql = "SELECT * FROM users WHERE email = ? AND pass = ? AND status = 1";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $email, $hash_pass);
+$stmt->execute();
+$result = $stmt->get_result();
+if(mysqli_num_rows($result) != 1){
     $outp = $resultCheck;
 } else {
     $outp = "success";
