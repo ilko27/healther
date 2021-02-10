0ae517538addf2adcf380da19afe4c1c687a7e49
SamuilG <samuilgeorgiev@abv.bg>
2021-02-10 21:37:10 +0200

    hacked it

--- index.php
+++ index.php
@@ -94,52 +94,66 @@ if (screen.width <= 1100) {
     </nav> -->
     <nav>
         <div class="nav-wrapper">
-        <a href="" class="brand-logo"><img id="header_img" src="images/big_healther_clear.png" alt="Healther"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a onclick="addSensor()">Add Sensor</a></li>
-            <li><a href="">Map</a></li>
-            <li><a href="php/logout.php">Logout</a></li>
-            <li><a href="">Account</а></li>
-        </ul>
+            <a href="#" class="brand-logo">
+                <img id="header_img" src="images/big_healther_clear.png" alt="Healther">
+            </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>
+                    <a href="php/logout.php">Logout</а>
+                </li>
+            </ul>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>
+                    <a onclick="addSensor()">Add Sensor</a>
+                </li>
+                <li>
+                    <a href="#">Map</a>
+                </li>
+                <li>
+                    <a href="#">Account</а>
+                </li>
+            </ul>
         </div>
     </nav>
     
 
     <div id="leftHalf">
+
     <?php
-    $userId = $_SESSION['userId'];
-    $sqlQuery = "SELECT * FROM `sensors` WHERE user_id = ".$userId;
-    $result = $conn -> query($sqlQuery);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $sensorId = $row['sensor_id'];
-            $sensorQuery = "SELECT * FROM sensorData WHERE sensor = ".$sensorId." ORDER BY datId DESC LIMIT 1";
-            $sensorReading= $conn -> query($sensorQuery);
-            $sensData = $sensorReading->fetch_assoc();
-            echo "<div class='row'>
-            <div class='col s12 m6'>
-              <div class='card blue-grey darken-1' onclick='getData(".$row['sensor_id'].")'>
-                <div class='card-content white-text'>
-                    <span class='card-title'>".$row['sensor_name']."</span>
-                    <table>
-                        <tr><td class='labelTd'>PM2.5</td><td class='numberTd'>23</td></tr>
-                        <tr><td class='labelTd'>Temperature</td><td class='numberTd'>".$sensData['temperatureC']."</td></tr>
-                        <tr><td class='labelTd'>Humidity</td><td class='numberTd'>".$sensData['humidity']."</td></tr>
-                        <tr><td class='labelTd'>Pressure</td><td class='numberTd'>".$sensData['pressure']."</td></tr>        
-                    </table>
-                </div>
-                <div class='card-action'>
-                    <a onclick='editSensor($sensorId)'>Edit</a>
-                    <a onclick='removeSensor($sensorId)'>Remove</a>
-                </div>
-              </div>
-            </div>
-          </div>";
-        }
+        $userId = $_SESSION['userId'];
+        $sqlQuery = "SELECT * FROM `sensors` WHERE user_id = ".$userId;
+        $result = $conn -> query($sqlQuery);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $sensorId = $row['sensor_id'];
+                $sensorQuery = "SELECT * FROM sensorData WHERE sensor = ".$sensorId." ORDER BY datId DESC LIMIT 1";
+                $sensorReading= $conn -> query($sensorQuery);
+                $sensData = $sensorReading->fetch_assoc();
+                echo "  <div class='row'>
+                            <div class='col s12 m6'>
+                                <div class='card blue-grey darken-1' onclick='getData(".$row['sensor_id'].")'>
+                                    <div class='card-content white-text'>
+                                        <span class='card-title'>".$row['sensor_name']."</span>
+                                        <table>
+                                            <tr><td class='labelTd'>PM2.5</td><td class='numberTd'>23</td></tr>
+                                            <tr><td class='labelTd'>Temperature</td><td class='numberTd'>".$sensData['temperatureC']."</td></tr>
+                                            <tr><td class='labelTd'>Humidity</td><td class='numberTd'>".$sensData['humidity']."</td></tr>
+                                            <tr><td class='labelTd'>Pressure</td><td class='numberTd'>".$sensData['pressure']."</td></tr>        
+                                        </table>
+                                    </div>
+                                        <div class='card-action'>
+                                        <a onclick='editSensor($sensorId)'>Edit</a>
+                                        <a onclick='removeSensor($sensorId)'>Remove</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>";
+            }
         } else {
-        echo "You don't have any sensors added. You can add one by clicking <a onclick='addSensor()' class='waves-effect waves-light btn'>Here</a>";
+            echo "You don't have any sensors added. You can add one by clicking <a onclick='addSensor()' class='waves-effect waves-light btn'>Here</a>";
         }
     ?>
+
     </div>
     
     <div id="rightHalf">
