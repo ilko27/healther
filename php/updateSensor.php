32e6b97d92466dd5df4d3709ecd3c6f4f768d42f
SamuilG <samuilgeorgiev@abv.bg>
2021-02-13 19:56:59 +0200

    update sensor name PrepSt

--- php/updateSensor.php
+++ php/updateSensor.php
@@ -7,11 +7,14 @@ $inInfo = json_decode(file_get_contents("php://input"));
 $sensorId = $inInfo->sensorId;
 $newName = $inInfo->newName;
 $user_id = $_SESSION['userId'];
-var_dump($sensorId, $newName, $user_id);
+// var_dump($sensorId, $newName, $user_id);
 
-$sql = "DELETE FROM `sensors` WHERE sensors.user_id = $user_id AND sensors.sensor_id = $sensorId";
-$conn->query($sql);
+$sql = "UPDATE `sensors` SET sensors.sensor_name = ? WHERE sensors.user_id = ? AND sensors.sensor_id = ?";
 
-$sql1 = "INSERT INTO `sensors`(`sensor_name`, `user_id`, `sensor_id`, `admin`) VALUES ('$newName', $user_id, $sensorId, 1)";
-$conn -> query($sql1);
+$stmt = mysqli_stmt_init($conn); 
+mysqli_stmt_prepare($stmt, $sql);
+mysqli_stmt_bind_param($stmt, "sii", $newName, $user_id, $sensorId);
+mysqli_stmt_execute($stmt);
+
+
 ?>
